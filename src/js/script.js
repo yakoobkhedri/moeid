b053326ee099141144f7ac9e1ff493a63b28a3a1
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-02-15 18:33:47 +0330

    commit message

--- src/js/script.js
+++ src/js/script.js
@@ -270,3 +270,22 @@ addressItem.forEach((item)=>{
     item.classList.add('active');
   })
 })
+
+// video player
+const playerVideo = document.getElementById("player-video");
+const playerbutton = document.getElementById("player-button");
+
+playerbutton.addEventListener("click", () => {
+  playerVideo.play();
+    playerVideo.setAttribute("controls", "");
+});
+
+playerVideo.addEventListener("play", () => {
+  playerbutton.style.visibility = "hidden";
+  playerbutton.style.opacity = "0";
+});
+
+playerVideo.addEventListener("pause", () => {
+  playerbutton.style.visibility = "visible";
+  playerbutton.style.opacity = "1";
+});
